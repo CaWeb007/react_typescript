a2f30df1c6839b3d23da738f5d09549f6c428f24
p.reutov <reutov@caweb.ru>
2023-07-12 17:47:05 +0800

    wrapped html elements & Omit(исключение)

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,8 @@ import {Profile} from "./components/auth/Profile";
 import {List} from "./components/generics/List";
 import {RandomNumber} from "./components/restriction/RandomNumber";
 import {Toast} from "./components/templateliterals/Toast";
+import {CustomButton} from "./components/html/Button";
+import {CustomInput} from "./components/html/Input";
 
 function App() {
     const personName = {
@@ -95,6 +97,10 @@ function App() {
         />
         <RandomNumber value={2} isPositive/>
         <Toast position={'center'}/>
+        <CustomButton variant={'primary'} onClick = {() => console.log('clicked')}>
+            Primary button
+        </CustomButton>
+        <CustomInput />
     </div>
     );
 }
